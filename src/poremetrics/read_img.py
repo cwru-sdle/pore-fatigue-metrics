4913e8874e581746f66162d70636bf40c5ee843a
Anthony Lino <aml334@case.edu>
2025-04-20 22:36:01 -0400

    beginning of tests

---
+++ src/poremetrics/read_img.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+def read_imgs():
+    test_data_path = Path(__file__).parent / "data" / "ex_white.png"
---
+++ tests/test_read_imgs.py
@@ -0,0 +1,8 @@
+from poremetrics import read_img
+
+def test_something():
+    shape = cv2.imread(test_data_path).shape
+
+
+
+test_something()
